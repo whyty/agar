6e697fce21b91037eb3f69c86c74afbc73801e91
MuTuLiCa <whyty@gmail.com>
2014-02-16 22:42:02 +0200

    Dinamic services - constructions

--- application/controllers/servicecontroller.php
+++ application/controllers/servicecontroller.php
@@ -5,10 +5,14 @@ class ServiceController extends Controller {
         
 	function index(){
            $this->set('title','Agar - Servicii &raquo; Constructii');
+           $content_i = $this->Service->query("SELECT * FROM `page` WHERE `type`='2'"); 
+             $this->set("articles_ci", $content_i);
 	}
 
 	function constructions(){
              $this->set('title','Agar - Servicii &raquo; Constructii');
+             $content_c = $this->Service->query("SELECT * FROM `page` WHERE `type`='2'"); 
+             $this->set("articles_c", $content_c);
 	}
 
 
--- application/views/service/constructions.php
+++ application/views/service/constructions.php
@@ -85,59 +85,23 @@ echo "<img class='banner_service' src='".URL."public/images/bannere/banner_const
 
 
 	<div class ='blocks'>
-		<div class='service_block'>
-			<div class = 'service_content_title'>
+            <?php
+                foreach($articles_c as $article){
+                    if($article['cols']!='yes'){
+                        echo '<div class="service_block">';
+                        if($article['title']!=''){
+                            echo "<div class = 'service_content_title'>
 				<div class='service_content_title_hr'><hr /></div>
-				CONSTRUCTII
-			</div>
-			<div class='slide_service'>
-				AGAR COMPANY este specializata in constructia de case si vile noi in stil traditional, modern, renovare de case vechi si apartamente precum si in lucrari de amenajari interioare, instalatiir electrice si sanitare.</br>
-				Suntem o firma de constructii cu experienta si dinamica, formata dintr-o echipa de profesionisti.</br>
-				Oferim competenta si echipamentele pentru toate tipurile de constructii, indiferent de dimensiune si complexitate, timp si buget, facand ca proiectul si visele dvs sa devina realitate.</br>
-				Pana la finalizarea proiectului exista o "supraveghere stricta" in toate etapele de constructive, oferim regulat rapoarte de lucru, atasat cu fotografii, pentru fiecare faza de constructie.</br>
-				Pentru fiecare din proiectele si lucrarile nostre vom folosi materiale de inalta calitate si vom acorda garantie atat pentru material cat si pentru serviciile prestate. </br>
-				Prin acest mod de lucru am reusit sa construim case sigure si functionale.
-			</div>
-		</div>
-
-		<div class='service_block'>
-			<div class = 'service_content_title'>
-				<div class='service_content_title_hr'><hr /></div>
-				RENOV&#x102RI
-			</div>
-			Renovarile de cladiri si apartamente istroice este una din ramurile in care echipa AGAR COMPANY a dobandit expertienta in decursul anilor.</br>
-			Pentru fiecare din lucrari sunt folosite materiale de inalta calitate se acorda garantie atat pentru material cat si pentru serviciile prestate.</br>
-			Specialistii echipei din departamentul de constructii, va vor oferi solutiile optime, astfel incat lucrarea de renovare sa fie conform standardelor estetice si functionale dorite.
-		</div>
-
-
-		<div class='service_block'>
-			<div class = 'service_content_title'>
-				<div class='service_content_title_hr'><hr /></div>
-				PROIECTARE
-			</div>
-			Echipa de arhitecti ai companiei examineaza cu atentie caietului de sarcini si va vor propune o gama larga de proiecte de arhitectura in functie de necesitatile dumneavoastra.</br>
-			Indiferent de natura constructiei, echipa AGAR COMPANY va ofera prin serviciul de proiecatre solutii pentru o gama larga de constructii: constructii civile (case la rosu si case la cheie) si industriale (hale/structuri) ori parti ale acestora lucrari de invelitori, sarpante, terase la constructii.
-		</div>
-
-		<div class='service_block'>
-			<div class = 'service_content_title'>
-				<div class='service_content_title_hr'><hr /></div>
-				AMENAJ&#x102RI INTERIOARE
-			</div>
-			Daca doriti amenajarea unui spatiu de lucru, a unei locuinte sau a unui spatiu de recreere, AGAR COMPANY e in masura sa va ajute sa decideti intre o varietate de propuneri si sugestii creative precum si functionale.</br>
-			Agar Company realizeaza o diversitate de amenajari interioare: lucrari de instalatii sanitare si electrice, lucrari de izolatii termice, fonice si anticorozive, lucrari de pardosire si placare a peretilor, lucrari de vopsitorie, zugraveli si ipsoserie, lucrari de tamplarie si dulgherie.
-		</div>
-
-		<div class='service_block'>
-			<div class = 'service_content_title'>
-				<div class='service_content_title_hr'><hr /></div>
-				CONSULTAN&#538&#x102
-			</div>
-			AGAR COMPANY va ofera consultanta si asistenta in: proiecte case, certificate de urbanism, autorizatii de construire, avize, studii geotehnice.</br>
-			De asemenea companai colaboreaza indeaproape cu producatori de materiale si firme specializate pentru a obtine intotdeauna cele mai bune rezultate la preturi competitive si ofera asistenta tehnica pe perioada executiei lucrarilor. 
-		</br></br></br></br></br></br></br>
-	</div>
+				" . $article['title'] . "
+			</div>";
+                        }
+                            echo "<div class='slide_service'>";
+                            echo $article['text'];
+                            echo '</div>';
+                        echo '</div>';
+                    }
+                }
+            ?>
 </div>
 
 </center>
