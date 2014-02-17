438c009c562c101aec894f0450f3a8d657cc103a
MuTuLiCa <whyty@gmail.com>
2014-02-17 06:20:48 +0200

    Service consultanta

--- application/views/service/consultanta.php
+++ application/views/service/consultanta.php
@@ -54,60 +54,22 @@ echo "<img class='banner_service' src='".URL."public/images/bannere/banner_consu
 
 echo"<div class='content_body'>";
 
+foreach ($articles_cs as $article){
+    if($article['cols']=='yes'){
+        echo "<div class='home_block'>";
+        if($article['title']!=""){
+            echo "<div class = 'home_content_title'>
+                    <div class='home_content_title_hr'><hr /></div>";
+            echo $article['title'];
+            echo "</div>";
+        }
+            echo "<div class='slide'>";
+            echo $article['text'];
+            echo "</div>";
+       echo "</div>";
+    }
+}
 ?>
-
-<!-- first block --> 
-
-<div class='home_block'>
-	<div class = 'home_content_title'>
-		<div class='home_content_title_hr'><hr /></div>
-		CONSULTANTA
-	</div>
-	<div class='slide'>
-		AGAR COMPANY va ofera consultanta in Managementul Calitatii si de Dezvoltare, instrument necesar pentru optimizarea productivitatii si a relatiei companiei dvs cu clientii si furnizorii sai.</br>
-		Pentru mai multe detalii, va rugam sa ne <div class='text_link'><a href=<?php echo URL."contact/"; ?> target='_blank'>contactati</a></div>.
-	</div>
-	
-</div>
-
-
-<!-- second block --> 
-
-<div class='home_block'>
-	<div class = 'home_content_title'>
-		<div class='home_content_title_hr'><hr /></div>
-		CERTIFICARI
-	</div>
-	<div class='slide'>
-		Compania noastra va ofera sprijin in obtinerea urmatoarelor certificate: </br>
-		Sisteme de Management al Calitatii - ISO 9001</br>
-		Sisteme de Management al Mediului - ISO 14001</br>
-		Sisteme de Management al Sanatatii si Securitatii Ocupationale - OHSAS 18001</br>
-		Sisteme de Management al Responsabilitatii Sociale – Social Accountability 8000</br>
-		Sisteme de Management al Securitatii Informatiei - ISO/IEC 27001</br>
-		Sisteme de Management al Energiei - ISO 16001</br>
-		Sistem de Management al Sigurantei Alimentelor – HACCP / ISO 22000</br>
-		Pentru mai multe detalii, va rugam sa ne <div class='text_link'><a href=<?php echo URL."contact/"; ?> target='_blank'>contactati</a></div>. </br>
-	</div>
-	
-</div>
-
-
-
-<!-- third block -->
-
-<div class='home_block'>
-	<div class = 'home_content_title'>
-		<div class='home_content_title_hr'><hr /></div>
-		AUDIT
-	</div>
-	<div class='slide'>
-		AGAR COMPANY realizeaza, in conformitate cu metodologii utilizate si recunoscute in Uniunea Europeana, Audituri de Mediu conform standardelor de referinta si cerintelor clientului.</br>
-		Pentru mai multe detalii, va rugam sa ne <div class='text_link'><a href=<?php echo URL."contact/"; ?> target='_blank'>contactati</a></div>.
-	</div>
-
-</div>
-</br></br></br></br></br></br></br>
 
 </center>
 
