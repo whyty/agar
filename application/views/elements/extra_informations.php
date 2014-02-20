092ee899650053c27e7a1405ba65eb8878ebc14d
MuTuLiCa <whyty@gmail.com>
2014-02-20 09:10:30 +0200

    Modifications

--- application/views/elements/extra_informations.php
+++ application/views/elements/extra_informations.php
@@ -53,10 +53,51 @@
 	<div class='facebook_actions'>
 		<div class="fb-like" data-href="https://www.facebook.com/AgarCompanySrl?fref=ts" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
 	</div>
+	<div class='google_button'>
+		<!-- Place this tag where you want the +1 button to render. -->
+		<div class="g-plusone" data-annotation="none"></div>
+
+		<!-- Place this tag after the last +1 button tag. -->
+		<script type="text/javascript">
+		(function() {
+			var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+			po.src = 'https://apis.google.com/js/platform.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		})();
+		</script>
+	</div>
+	<div class='twitter_button'>
+		<a href="https://twitter.com/agarcompany" class="twitter-follow-button" data-show-count="false">Follow @agarcompany</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+	</div>
 </div>
 
 
 <div class='extra_information_block'>
-	<div class='extra_information_block_title'>Curs valutar:</div>
+	<div class='extra_information_block_title' id='currency_block_title'>CURS VALUTAR :</div>
+	<div class='currency_info'>
+		<!-- Simple Currency Rates Table START -->
+		<link rel="stylesheet" type="text/css" href="http://www.exchangerates.org.uk/widget/ER-SCRT2-css.php?w=180&nb=2&bdrc=F1EFEE&mbg=F1EFEE&fc=333333&tc=333333" media="screen" />
+		<div id="erscrt2"><div id="erscrt2-widget"></div><div id="erscrt2-infolink"><a href="http://www.exchangerates.org.uk/Romanian-Leu-RON-currency-table.html" target="_new" ><img src='http://www.exchangerates.org.uk/widget/logo-333333.png' alt='ExchangeRates.org.uk'></a></div>
+		<script type="text/javascript">	
+		var tz = 'userset';
+		var w = '180';
+		var mc = 'RON';
+		var nb = '2';
+		var c1 = 'EUR';
+		var c2 = 'USD';
+		var t = '';
+		var tc = '333333';
+		var bdrc = 'F1EFEE';
+		var mbg = 'F1EFEE';
+		var fc = '333333';
+
+		var ccHost = (("https:" == document.location.protocol) ? "https://www." : "http://www.");
+		document.write(unescape("%3Cscript src='" + ccHost + "exchangerates.org.uk/widget/ER-SCRT2-1.php' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+	</div>
+	<!-- Simple Currency Rates Table END -->
 </div>
+</div>
+
 
