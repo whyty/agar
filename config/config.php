4a865f52fb05b4efda0ef17ffb2ad72c19b2ab00
MuTuLiCa <whyty@gmail.com>
2014-03-04 11:57:26 +0200

    Final settings

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
 /** Configuration Variables **/
 
 define ('DEVELOPMENT_ENVIRONMENT',true);
-define('URL', 'http://agar.masterserver.org//');
-define('DB_NAME', 'agar');
-define('DB_USER', 'agaruser');
-define('DB_PASSWORD', 'hLrK6DqEmzG64faM');
+define('URL', 'http://agarcompany.com/');
+define('DB_NAME', 'agarcomp_db');
+define('DB_USER', 'agarcomp_user');
+define('DB_PASSWORD', 'agar123$%');
 define('DB_HOST', 'localhost');
